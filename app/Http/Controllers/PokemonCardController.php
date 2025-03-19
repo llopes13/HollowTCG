ebf34f3fae9dc78f0348ae7e9a2f3b9fb9c943d6
llopes <luiss.lopes13@gmail.com>
2025-03-20 00:00:45 +0100

    Cartas puestas en la base de datos

--- app/Http/Controllers/PokemonCardController.php
+++ app/Http/Controllers/PokemonCardController.php
@@ -10,9 +10,11 @@ class PokemonCardController extends Controller
 {
     public function fetchAndStore()
     {
+        set_time_limit(120);
+        for ($i = 1; $i < 10; $i++) {
         $response = Http::withHeaders([
             'X-Api-Key' => '1f2d482b-ab15-47f8-a444-ef88ec023590'
-        ])->get('https://api.pokemontcg.io/v2/cards?pageSize=8');
+        ])->get('https://api.pokemontcg.io/v2/cards?page='.$i);
 
         if ($response->failed()) {
             return response()->json(['error' => 'Error al obtener las cartas'], 500);
@@ -20,8 +22,10 @@ class PokemonCardController extends Controller
 
         $cards = $response->json()['data'];
 
+
         foreach ($cards as $card) {
-            $price = $card['tcgplayer']['prices']['normal']['market'] ?? null;
+            $price = $card['cardmarket']['prices']['trendPrice'] ?? $card['tcgplayer']['prices']['holofoil']['market'] ?? $card['tcgplayer']['prices']['normal']['low'] ?? $card['tcgplayer']['prices']['holofoil']['low'] ?? null;
+
 
             PokemonCard::updateOrCreate(
                 ['card_id' => $card['id']],
@@ -32,6 +36,7 @@ class PokemonCardController extends Controller
                 ]
             );
         }
+    }
 
         return response()->json(['message' => 'Cartas actualizadas']);
     }
